102e0ad223117f8d78dc103834d6284983e9ddae
Ryan <ryan.simao72.gmail.com>
2026-05-04 22:42:55 -0300

    Fazer

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Entre com a medida (em metros): ");
+decimal metros = Convert.ToDecimal (Console.ReadLine());
+
+Console.WriteLine("--- Equivalência ---");
+
+decimal cm = Convert.ToDecimal (metros ) * 100;
+
+Console.WriteLine(cm);
